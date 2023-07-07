f36c32305b40aed7a27300f3367303f762fd560c
Bereket Tadesse <bekimehari@gmail.com>
2023-07-06 20:37:17 -0400

    fixed import package

--- tests/test.py
+++ tests/test.py
@@ -1,5 +1,5 @@
-from autoRecord_Bereket_Tadesse import functions
-
+import sys
+sys.path.insert(0, 'C:/Users/Bereket/Desktop/random coding practice/gITHUB REPOS/pathologyDataManagement/autoRecord')
+from functions import add_one
 add_one()
-
 
